fdbba457d544d8bf95275de8a642e3a6e18af315
MohShalaby92 <mohammedshalaby92@gmail.com>
2025-02-25 10:49:52 -0800

    API Advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
 
     headers = {"User-Agent": "MyRedditBot/1.0"}
 
-    url = f"https://www.reddit.com/r/{}/about.json"
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
     response = requests.get(url, headers-headers, allow_redirects=False)
     if response.status_code != 200:
