90450b893ae0a2f98b8913335d91474fe0277385
MohShalaby92 <mohammedshalaby92@gmail.com>
2025-02-25 11:04:15 -0800

    API Advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,6 +3,7 @@
 queries the reddit API and prints the titles of the first 10 hot posts for a given subreddit.
 if the subreddit is invalid, prints None.
 """
+import requests
 
 def top_ten(subreddit):
 
