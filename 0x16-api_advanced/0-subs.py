d1740af683afa88d50da021213cb7b23d18b00cb
MohShalaby92 <mohammedshalaby92@gmail.com>
2025-02-25 10:52:10 -0800

    API Advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,7 @@ def number_of_subscribers(subreddit):
 
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
-    response = requests.get(url, headers-headers, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         return 0
 
